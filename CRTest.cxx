614c40f99360e02890f8541671c2691ac535313d
yatowoo <torrence@mail.ustc.edu.cn>
2017-04-03 22:13:01 +0800

    NEW exe arguments handle

--- CRTest.cxx
+++ CRTest.cxx
@@ -18,55 +18,75 @@
 #include "SysMessenger.hh"
 #include "PhysicsList.hh"
 
-#ifdef G4VIS_USE
-#include "G4VisExecutive.hh"
-#endif
 #include "G4RunManager.hh"
 #include "G4UImanager.hh"
 #include "G4UIExecutive.hh"
+#include "G4VisExecutive.hh"
 
 #include "G4VModularPhysicsList.hh"
-#include "FTFP_BERT.hh"
 #include "G4StepLimiterPhysics.hh"
-
-#include "G4GDMLParser.hh"
 
 #include "globals.hh"
 #include "G4ios.hh"
+#include<cstdlib>
+
+void Usage(){
+	G4cout << " Usage : ./CRTest [gdml] [mac] [output] [seed]"
+		<< G4endl;
+}
 
 int main (int argc, char** argv){
-    
-    G4cout << G4endl << "CRTest Start!"
-        << G4endl << G4endl;
-    
+       
     G4String gdmlFileName = "./mac/default.gdml";
-    G4String rootFileName = "CRTest";
+    G4String macroFileName = "./mac/run.mac";
+	G4String rootFileName = "CRTest";
+	G4int rndFactor = 1;
+	G4bool uiUse = true;
+	G4bool visUse = true;
+
+	// Handle Arguments
+	switch(argc){
+	case 5:
+		rndFactor = std::atoi(argv[4]) + 1;
+	case 4:
+		rootFileName = argv[3];
+	case 3:
+		macroFileName = argv[2];
+		visUse = false;
+		uiUse = false;
+	case 2:
+		gdmlFileName = argv[1];
+	case 1:break;
+	default:
+		Usage();
+		exit(1);
+	}
+#ifndef G4UI_USE
+	uiUse = false;
+#endif
+#ifndef G4UI_USE
+	visUse = false;
+#endif
 
     // UI Session
-#ifdef G4UI_USE
     G4UIExecutive* ui = NULL;
-    if (argc < 3)
+    if (uiUse)
         ui = new G4UIExecutive(argc, argv);
-    // else in Batch mode
-    if(argc > 1)
-        gdmlFileName = argv[1];
-#endif
 
     // Random
     G4Random::setTheEngine(new CLHEP::RanecuEngine);
-    srand(time(NULL));
-    G4Random::setTheSeed(rand(),3);
+	G4Random::setTheSeed(time(NULL)*rndFactor,3);
 
     // Run manager
     G4RunManager* runManager = new G4RunManager;
-    // User defined classes 
+    
+    G4cout << G4endl << "[***] WELCOME - CRTest Start!" << G4endl << G4endl;
+	
+	// User defined classes 
         // Detector Construction
     runManager->SetUserInitialization(
         new GdmlConstruction(gdmlFileName));
-    //runManager->SetUserInitialization(new SysConstruction());
     
-    //G4VModularPhysicsList* physicsList = new FTFP_BERT;
-    //physicsList->RegisterPhysics(new G4StepLimiterPhysics());
     G4VModularPhysicsList* physicsList = new PhysicsList;
     runManager->SetUserInitialization(physicsList);
     
@@ -75,38 +95,40 @@ int main (int argc, char** argv){
     SysMessenger* messenger = new SysMessenger(runManager);
     // Visualization Manager
 		// TODO : ADD verbosiry option and if/else by arg. mode
-#ifdef G4VIS_USE
-    G4VisManager* visManager = new G4VisExecutive("error");
-    visManager->Initialize();
-#endif
+    G4VisManager* visManager = NULL;
+	if(visUse){
+		visManager = new G4VisExecutive("error");
+		visManager->Initialize();
+	}
 
     // UIManager
     G4UImanager* uiManager = G4UImanager::GetUIpointer();
     if( argc > 2){
+        // Set outpu .root file name from argument
+        G4String command = "/analysis/setFileName ";
+		G4String fileName = rootFileName;
+        uiManager->ApplyCommand(command+fileName);
         // Execute macro from argument
-        G4String command = "/control/execute ";
-        G4String fileName = argv[2];
+        command = "/control/execute ";
+		fileName = macroFileName;
         uiManager->ApplyCommand(command+fileName);
     }
-    else{
-        // Execute Initialization Macro by UIManager
-        uiManager->ApplyCommand("/control/execute ./mac/init.mac");
-#ifdef G4UI_USE
-        if(ui->IsGUI())
-            uiManager->ApplyCommand("/control/execute ./mac/gui.mac");
-        // Start Session
-        ui->SessionStart();
-#endif
-    }// mode case
+	else{
+		// Execute Initialization Macro by UIManager
+		uiManager->ApplyCommand("/control/execute ./mac/init.mac");
+		if(uiUse){
+			if(ui && ui->IsGUI())
+				uiManager->ApplyCommand("/control/execute ./mac/gui.mac");
+			// Start Session
+			ui->SessionStart();
+		}
+
+	}// mode case
     
     // delete
-#ifdef G4UI_USE
-    delete ui;
-#endif
-    delete messenger;
-#ifdef G4VIS_USE
-    delete visManager;
-#endif
+	if(uiUse) delete ui;
+	if(visUse)	delete visManager;
+	delete messenger;
     delete runManager;
 
     return 0;
