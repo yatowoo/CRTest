75de7f66fe030e636249ebf01ba70a70a8613c15
yatowoo <torrence@mail.ustc.edu.cn>
2017-04-06 15:58:18 +0800

    CHANGE OpRecorder count method

--- src/StepAction.cc
+++ src/StepAction.cc
@@ -16,9 +16,6 @@
 
 #include "G4ParticleDefinition.hh"
 #include "G4OpticalPhoton.hh"
-
-#include "G4ProcessManager.hh"
-#include "G4Scintillation.hh"
 #include "G4OpBoundaryProcess.hh"
 
 #include "G4SystemOfUnits.hh"
@@ -53,57 +50,53 @@ void StepAction::UserSteppingAction(const G4Step *aStep)
     //
     // Boundary Check
     //
+	ana::VertexType type = ana::Final;
     if (thePostPoint->GetStepStatus() == fGeomBoundary)
-    {
+	{
         assert(theProcess->GetProcessName() == "OpBoundary");
         G4OpBoundaryProcess *boundary = (G4OpBoundaryProcess *)theProcess;
-        if (thePrePV->GetName() == "Detector_PV" &&
-            thePostPV->GetName() == "Groove_PV")
-        {
-            ana::FillVertexForEvent(theTrack, ana::VertexType::Scint2Groove);
-            Recorder->nScint2Groove += 1;
-            return;
-        }
-        else if (thePrePV->GetName() == "Groove_PV" &&
-                 thePostPV->GetName() == "Cladding_PV")
-        {
-            ana::FillVertexForEvent(theTrack, ana::VertexType::Groove2Cladding);
-            Recorder->nGroove2Cladding += 1;
-            return;
-        }
-        else if (thePrePV->GetName() == "Cladding_PV" &&
-                 thePostPV->GetName() == "Core_PV")
-        {
-            ana::FillVertexForEvent(theTrack, ana::VertexType::Cladding2Core);
-            Recorder->nGroove2Cladding += 1;
-            return;
-        }
+		G4OpBoundaryProcessStatus status = boundary->GetStatus();
+		G4bool gotThrough = 
+			(status == Transmission || status == FresnelRefraction);
+		if(gotThrough){
+			// OpPthoton got through boundary
+			if (thePrePV->GetName() == "Detector_PV" &&
+				thePostPV->GetName() == "Groove_PV")
+			{
+				type = ana::Scint2Groove;
+				Recorder->nScint2Groove ++;
+
+			}
+			else if (thePrePV->GetName() == "Groove_PV" &&
+				thePostPV->GetName() == "Cladding_PV")
+			{
+				type = ana::Groove2Cladding;
+				Recorder->nGroove2Cladding += 1;
+			}
+			else if (thePrePV->GetName() == "Cladding_PV" &&
+				thePostPV->GetName() == "Core_PV")
+			{
+				type = ana::Cladding2Core;
+				Recorder->nCladding2Core += 1;
+			}
+			// TODO : REMOVE after GDML setup completed
+			else if (thePrePV->GetName() == "Groove_PV" &&
+				thePostPV->GetName() == "Core_PV")
+			{
+				type = ana::Groove2Cladding;
+				Recorder->nGroove2Cladding += 1;
+			}
+		}
         else if (thePrePV->GetName() == "Core_PV" &&
                  thePostPV->GetName() == "PMT_PV")
         {
-            ana::FillVertexForEvent(theTrack, ana::VertexType::Fiber2Pmt);
+			// OpPhoton hit PMT photocathode
+            type = ana::Fiber2Pmt;
             Recorder->nCore2PMT += 1;
-            if (boundary->GetStatus() == Detection)
-                Recorder->nDetection += 1;
-
-            return;
+            if (status == Detection)
+				Recorder->nDetection += 1;
         }
-        // TODO : REMOVE after GDML setup completed
-        else if (thePrePV->GetName() == "Groove_PV" &&
-                 thePostPV->GetName() == "Core_PV")
-        {
-            ana::FillVertexForEvent(theTrack, ana::VertexType::Groove2Cladding);
-            Recorder->nGroove2Cladding += 1;
-            return;
-        }
-        else if (thePrePV->GetName() == "Core_PV" &&
-                 thePostPV->GetName() == "World_PV")
-        {
-            ana::FillVertexForEvent(theTrack, ana::VertexType::Fiber2Pmt);
-            Recorder->nCore2PMT += 1;
-            //Recorder->nDetection +=1;
-            return;
-        }
+		// For Debug boundary details
         else if (thePrePV->GetName() == "Core_PV" &&
                  thePostPV->GetName() == "Groove_PV")
         {
@@ -114,6 +107,8 @@ void StepAction::UserSteppingAction(const G4Step *aStep)
             return;
         }
     }
+	if(type != ana::Final)
+		ana::FillVertexForEvent(theTrack, type);
 }
 
 G4bool StepAction::BoundaryStats(G4OpBoundaryProcess *boundary)
